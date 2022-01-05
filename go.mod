1d2431707a8be78b289e091b7425481dffb27fc8
seggi <seggimarugira@gmail.com>
2022-01-05 07:58:48 -0500

    user models & repository

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module fuko_backend
 
 go 1.17
 
+require github.com/joho/godotenv v1.4.0
+
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.7 // indirect
@@ -13,15 +15,14 @@ require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.3 // indirect
-	github.com/joho/godotenv v1.4.0
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gorm.io/driver/mysql v1.2.2 // indirect
 	gorm.io/gorm v1.22.4 // indirect
--- models/users.go
+++ models/users.go
@@ -5,35 +5,55 @@ import (
 )
 
 // Model
-type Users struct {
+type User struct {
 	ID        int64     `gorm:"primary_key;auto_incriment" json:"id"`
-	FirstName string    `gorm:"size:50" json:"fitst_name"`
-	LastName  string    `gorm:"size:50" json:"last_name"`
+	FirstName string    `json:"fitst_name"`
+	LastName  string    `json:"last_name"`
 	Sex       string    `gorm:"size:10" json:"sex"`
-	Age       string    `gorm:"size:20" json:"age"`
-	Email     string    `gorm:"size:200" json:"email"`
-	Phone     string    `gorm:"size:50" json:"phone"`
-	Username  string    `gorm:"size:200" json:"username"`
-	Password  string    `gorm:"size:50" json:"password"`
+	Age       string    `json:"age"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	IsActive  bool      `json:"is_active"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
 
-func (users *Users) TableName() string {
-	return "users"
+// Returns the table name of the Model
+func (users *User) TableName() string {
+	return "user"
 }
 
-func (users *Users) ResponseMap() map[string]interface{} {
+// Request Binding for User Login
+type UserLogin struct {
+	Email    string `form:"email" binding:"required"`
+	Password string `form:"passsword" binding:"required"`
+}
+
+// Request Binding for User Register
+type UserRegister struct {
+	Phone     string `form:"phone" json:"phone"`
+	Email     string `form:"email" json:"email" binding:"required"`
+	Username  string `form:"username" json:"username"`
+	Password  string `form:"password" json:"password" binding:"required"`
+	FirstName string `form:"first_name"`
+	LastName  string `form:"last_name"`
+	Sex       string `form:"sex"`
+}
+
+func (user *User) ResponseMap() map[string]interface{} {
 	resp := make(map[string]interface{})
-	resp["id"] = users.ID
-	resp["first_name"] = users.FirstName
-	resp["last_name"] = users.LastName
-	resp["sex"] = users.Sex
-	resp["age"] = users.Age
-	resp["email"] = users.Email
-	resp["phone"] = users.Phone
-	resp["username"] = users.Username
-	resp["password"] = users.Password
+	resp["id"] = user.ID
+	resp["first_name"] = user.FirstName
+	resp["last_name"] = user.LastName
+	resp["sex"] = user.Sex
+	resp["age"] = user.Age
+	resp["email"] = user.Email
+	resp["phone"] = user.Phone
+	resp["username"] = user.Username
+	resp["password"] = user.Password
+	resp["is_active"] = user.IsActive
 
 	return resp
 }
